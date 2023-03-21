dd0d12f7c7fc45382c89312574345355c0e6bd4d
Rafael Alves <rbalves.ads@gmail.com>
2023-03-21 10:44:51 -0300

    test: validate empty pattern

--- src/index.test.ts
+++ src/index.test.ts
@@ -35,6 +35,10 @@ test('should stop masking when char not match mask and avoid ends with symbol', 
 	expect(result).toEqual('1-2')
 })
 
-test('should keep mask non pattern char on value', function() {
+test('should keep mask non pattern char on value', function () {
 	expect(mask('+55 (48) 888-8', '+55 (99) 999-9999')).toEqual('+55 (48) 888-8')
+})
+
+test('should return empty value when pattern is empty', function () {
+	expect(mask('+55 (48) 888-8', '')).toEqual('')
 })
