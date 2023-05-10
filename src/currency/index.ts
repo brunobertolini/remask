5288e15f3f5bc0f60d2d53c778cb39d1b4c0c768
Pedro Pereira <p.pedrospj@gmail.com>
2023-05-10 02:48:16 -0300

    docs: add code documentation to currency

--- src/currency/index.ts
+++ src/currency/index.ts
@@ -1,15 +1,22 @@
 interface CurrencyMaskProps {
+	/** Locale used to format. Refer to {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl#locales_argument MDN Docs} for futher explanation.*/
 	locale: string | string[]
+	/** The currency to use in currency formatting. Possible values are the `ISO 4217` currency codes. Refer to {@link https://wikipedia.org/wiki/ISO_4217 Wikipedia} to see all codes.*/
 	currency: string
+	/** The amount to be formatted. */
 	value: number | bigint
 }
 
 interface CurrencyUnmaskProps {
+	/** The locale used to format the amount. */
 	locale: string | string[]
+	/** The currency used to format the amount. */
 	currency: string
+	/** The formatted amount. */
 	value: string
 }
 
+/** Formats a given amount by the `locale` and `currency` provided.*/
 export const mask = ({
 	locale,
 	currency,
@@ -23,6 +30,7 @@ export const mask = ({
 	return format(value)
 }
 
+/** Given a `formatted amount`, `locale` and `currency`, returns the numeric value it corresponds to.*/
 export const unmask = ({ locale, currency, value }: CurrencyUnmaskProps) => {
 	const formatter = new Intl.NumberFormat(locale, {
 		style: 'currency',
